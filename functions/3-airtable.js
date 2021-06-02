8425110723ce9d93aa8c82c4d2ca8dd008b1379a
RodCerB <rodrigo.cbraga@hotmail.com>
2021-06-02 13:21:42 -0300

    add informaçao como add key do env no netlify

--- functions/3-airtable.js
+++ functions/3-airtable.js
@@ -1,5 +1,8 @@
 // Observações importantes: A key do airtable ela fica exposta, dessa forma qualquer pessoa pode chegar e deletar todos os seus produtos. Para evitar isso, não vamos utilizar a api diretamente no frontend, vamos usar o serveless, além disso vamos usar o ENV para guardar nossa key, por isso só subi no github após definir isso corretamente.
 // Resumindo: Se fizéssemos o api request diretamente no frontend, só que usando o env, o netlify não conseguiria ler a api pq não entenderia a key vindo do env, para isso precisamos puxar ela para o serverless, dessa forma armazenando apenas os dados da api, e aí sim passando para o frontend, deixando dessa forma 'legível' para o netlify
+
+// OBS: Para add o env key no netlify, precisa ir em site setings -> Build & deploy -> enviroment variables -> lá add
+
 // aqui nós estamos usando o module airtable-node que facilita na hora de fazer o request da api, por isso não estamos usando métodos tradicionais de fetch api etc
 require('dotenv').config()
 const Airtable = require('airtable-node');
