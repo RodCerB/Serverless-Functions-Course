91e63c8683dd840e0995f703b00e6ea53d67ada4
RodCerB <rodrigo.cbraga@hotmail.com>
2021-06-11 13:05:09 -0300

    Feito a pagina de single product, trazendo as informacoes necessarias

---
+++ REACT/src/Product.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react'
+import Axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
+
+const Product = () => {
+    const [loading, setLoading] = useState(true)
+    const [product, setProduct] = useState(null)
+    const {productID} = useParams()
+
+    const fetchData = async () => {
+        try {
+            const {data} = await axios.get(`/api/products?id=${productID}`)
+            setProduct(data)
+        } catch (error) {
+
+        }
+        setLoading(false)
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    if(loading){
+        return (
+        <section className='section section-center'>
+            <h2>Loading...</h2>
+        </section>)
+    }
+
+    const {fields} = product
+    const {name, desc, price, image} = fields
+
+    return (
+        <section className='section section-center'>
+            <Link to='/' className='link'>
+                Back Home
+            </Link>
+            <div>
+                <div className="title">
+                    <h2>{name}</h2>
+                    <div className="title-underline"></div>
+                </div>
+                <article className='single-product'>
+                    <img src={imag[0].url} alt={name} className='single-product-img'/>
+                
+                    <div>
+                        <h5>{name}</h5>
+                        <h5 className='price'>$ {price}</h5>
+                        <p>{desc}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default Product
