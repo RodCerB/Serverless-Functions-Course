69e1a989b9670e995223eebd526082776fe1bd9f
RodCerB <rodrigo.cbraga@hotmail.com>
2021-06-11 13:24:39 -0300

    Add algumas coisas que faltavam para o deploy

--- REACT/src/App.js
+++ REACT/src/App.js
@@ -1,6 +1,12 @@
 import Basic from './Basic'
 import Airtable from './Airtable'
 
+// Para o Deploy, nós precisamos inserir algumas informações no netlify.toml, na parte de build. Como o
+// command
+// publish
+// Além disso se o projeto tiver um redirect calibrado para /*, nós devemos setar um outro redirects nele, nesse projeto nao precisaria, mas de qualquer forma vou add lá
+
+
 function App() {
   return <>
     <Basic />
